5477437e8d1153d05841bc655f4072c5abf3d4a3
Justin Thomas <justin.thomas1@gmail.com>
2013-05-04 00:10:29 -0700

    more tests

--- test/sider-search-test.js
+++ test/sider-search-test.js
@@ -11,25 +11,57 @@ describe('search',function() {
 		y:123,
 		id:2
 	};
-	var k = 'sdr:n:2';
+	var doc = {
+		x: 'this rad tubes bro',
+		y: 12,
+		id: 4
+	};
+	var k1 = 'sdr:n:2';
+	var k2 = 'sdr:n:4';
 
 	before(function(done) {
 		var indexer = new sider.Indexer();
 		indexer.store(obj);
+		indexer.store(doc);
 		done();
 	});
 
-	it('Should search for tokens.',function(done) {
+	it('Should search for single token.',function(done) {
 		var searcher = new sider.Searcher();
-		searcher.getIds('this rad',function(replies) {
-			assert.deepEqual(replies,['sdr:n:2']);
+		searcher.getIds('rad',function(replies) {
+			console.log(replies);
+			assert.sameMembers(replies,[k1,k2]);
 			done();
 		});
 	});
 
+	it('Should search for two tokens and fail.',function(done) {
+		var searcher = new sider.Searcher();
+		searcher.getIds('some bro',function(replies) {
+			assert.ok(replies.length === 0);
+			done();
+		});
+
+	});
+
+	it('Should search for two tokens and succeed.',function(done) {
+		var searcher = new sider.Searcher();
+		searcher.getIds('this rad',function(replies) {
+			console.log(replies);
+			assert.sameMembers(replies,[k1,k2]);
+			done();
+		});
+
+	});
+
+	it('Should search handle null.');
+
 	after(function(done) {
 		var indexer = new sider.Indexer();
-		indexer.remove(k,done);
+		indexer.remove(k1,function() {
+			indexer.remove(k2,done);	
+		});
+
 	});
 
 });
